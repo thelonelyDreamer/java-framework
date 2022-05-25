492ea6800d4c4fe9cc73c9514a04309283a36db6
cleboy <1192136710@qq.com>
2022-05-25 20:33:30 +0800

    ok

--- swagger/springboot-swagger2-demos/knife4j-swagger/src/main/java/com/felixwc/springboot/swagger2/config/SwaggerConfigCostumer.java
+++ swagger/springboot-swagger2-demos/knife4j-swagger/src/main/java/com/felixwc/springboot/swagger2/config/SwaggerConfigCostumer.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -18,22 +18,23 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfigCostumer{
-    @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .enable(true)
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        Docket docket=new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        //.title("swagger-bootstrap-ui-demo RESTful APIs")
+                        .description("# swagger-bootstrap-ui-demo RESTful APIs")
+                        .termsOfServiceUrl("http://www.xx.com/")
+                        .contact(new Contact("shine","url","email"))
+                        .version("1.0")
+                        .build())
+                //分组名称
+                .groupName("2.X版本")
                 .select()
+                //这里指定Controller扫描包路径
                 .apis(RequestHandlerSelectors.basePackage("com.felixwc.springboot.swagger2"))
                 .paths(PathSelectors.any())
                 .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("···接口文档")
-                .description("")
-                .version("1.0")
-                .build();
+        return docket;
     }
 }
